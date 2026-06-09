96f6464c20931706cf97d3d402d61bab055f7816
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-09 23:49:33 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include "tracker.h"
 
 using namespace std;
 
 int main() {
-    Tracker tracker;
+    Tracker t;
 
     int choice;
     string category;
@@ -14,24 +14,24 @@ int main() {
         cout << "\n===== SMART EXPENSE TRACKER =====\n";
         cout << "1. Add Expense\n";
         cout << "2. Show Report\n";
-        cout << "3. Undo Last Expense\n";
+        cout << "3. Undo Last\n";
         cout << "4. Exit\n";
         cout << "Enter choice: ";
         cin >> choice;
 
         if (choice == 1) {
-            cout << "Enter category: ";
+            cout << "Category: ";
             cin >> category;
-            cout << "Enter amount: ";
+            cout << "Amount: ";
             cin >> amount;
 
-            tracker.addExpense(category, amount);
+            t.addExpense(category, amount);
         }
         else if (choice == 2) {
-            tracker.showReport();
+            t.showReport();
         }
         else if (choice == 3) {
-            tracker.undoLast();
+            t.undoLast();
         }
         else if (choice == 4) {
             cout << "Exiting...\n";
