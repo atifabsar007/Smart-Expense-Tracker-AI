4742eb599a346af655156d2d476ca229838ed7fd
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-11 20:26:31 +0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import Dashboard from "./Dashboard";
+
+function App() {
+  return (
+    <div>
+      <h1>💰 Smart Expense Tracker AI</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
