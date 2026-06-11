4a75ae0bedad6b21d5b9803c00b040760318c514
Md. Atif Absar <mr.atif.absar@gmail.com>
2026-06-11 20:05:23 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 using namespace std;
 
 int main() {
+
+    Auth auth;
+
     string user, pass;
 
     cout << "=== SMART EXPENSE TRACKER ===\n";
@@ -16,9 +19,10 @@ int main() {
     cout << "Password: ";
     cin >> pass;
 
-    if (!loginUser(user, pass)) {
+    if (!auth.loginUser(user, pass)) {
+
         cout << "User not found. Creating account...\n";
-        registerUser(user, pass);
+        auth.registerUser(user, pass);
     }
 
     Tracker tracker(user);
@@ -26,20 +30,29 @@ int main() {
     tracker.setBudget(5000);
 
     int choice;
-    string cat;
+    string cat, note;
     double amt;
 
     while (true) {
-        cout << "\n1.Add Expense\n2.Show Report\n3.Export CSV\n4.Exit\n";
+
+        cout << "\n1. Add Expense";
+        cout << "\n2. Show Report";
+        cout << "\n3. Exit\n";
+        cout << "Choice: ";
         cin >> choice;
 
         if (choice == 1) {
+
             cout << "Category: ";
             cin >> cat;
+
             cout << "Amount: ";
             cin >> amt;
 
-            tracker.addExpense(cat, amt);
+            cout << "Note: ";
+            cin >> note;
+
+            tracker.addExpense(cat, amt, note);
         }
 
         else if (choice == 2) {
@@ -47,15 +60,16 @@ int main() {
         }
 
         else if (choice == 3) {
-            exportCSV(user, tracker.getAll());
-            cout << "✔ Exported!\n";
+
+            saveData(user, tracker.getAll());
+            cout << "Data saved. Goodbye!\n";
+            break;
         }
 
-        else break;
+        else {
+            cout << "Invalid choice!\n";
+        }
     }
-
-    saveData(user, tracker.getAll());
-    cout << "Data saved.\n";
 
     return 0;
 }
